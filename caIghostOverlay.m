da8411133569c8638b124f029b9ee43a66f9aeba
Konlin <konlin.shen@gmail.com>
2013-06-05 16:29:30 -0400

    fixed the axes

--- caIghostOverlay.m
+++ caIghostOverlay.m
@@ -1,3 +1,8 @@
+%overlays the boundaries of the colormaps for dorsal and ventral calcium
+%activity
+%by Konlin Shen
+%06/05/13
+
 function caIghostOverlay(dorsal_brightness_filtered, ventral_brightness_filtered)
 %save the images and load them into the workspace
 imwrite(dorsal_brightness_filtered, 'dorsalcolormap.jpg');
@@ -7,12 +12,15 @@ dorsalcolormap=imread('dorsalcolormap.jpg');
 ventralcolormap=imread('ventralcolormap.jpg');
 
 %create masks from binary image
-dMask=im2bw(dorsalcolormap, .75);
-vMask=im2bw(ventralcolormap, .75);
+threshold=0.7;
+
+dMask=im2bw(dorsalcolormap, threshold);
+vMask=im2bw(ventralcolormap, threshold);
 
 %use masks to find boundaries
 figure;
 imagesc(dorsalcolormap);
+title('Dorsal Brightness Ratio');
 dbndries=bwboundaries(dMask,'noholes');
 dbndry=zeros(1,2);
 for k=1:length(dbndries)
@@ -23,14 +31,15 @@ for k=1:length(dbndries)
     dbndry=[dbndry;dbndry(1:n,:)];
     dbnd=filter(filt,1,dbndry);
     dboundary=dbnd(n+1:end,:);
+    dbCell{k}=dboundary;
     hold on;
-    for k = 1:length(dbndries)
-        plot(dboundary(:,2), dboundary(:,1), 'w', 'LineWidth', 2)
-    end
+    plot(dboundary(:,2), dboundary(:,1), 'w', 'LineWidth', 2)
+
 end
 
 figure;
 imagesc(ventralcolormap);
+title('Ventral Brightness Ratio');
 vbndries=bwboundaries(vMask,'noholes');
 vbndry=zeros(1,2);
 for k=1:length(vbndries)
@@ -41,22 +50,26 @@ for k=1:length(vbndries)
     vbndry=[vbndry;vbndry(1:n,:)];
     vbnd=filter(filt,1,vbndry);
     vboundary=vbnd(n+1:end,:);
+    vbCell{k}=vboundary;
     hold on;
-    for k = 1:length(vbndries)
-        plot(vboundary(:,2), vboundary(:,1), 'w', 'LineWidth', 2)
-    end
+    plot(vboundary(:,2), vboundary(:,1), 'w', 'LineWidth', 2)
+
 end
 
 %overlay boundaries
 figure;
 hold on;
-for k = 1:length(dbndries)
-    plot(dboundary(:,2), dboundary(:,1), 'w', 'LineWidth', 2, 'Color', 'g')
+set(gca,'YDir','reverse');
+
+for k = 1:length(dbCell)
+    dboundary=dbCell{k};
+    plot(dboundary(:,2), dboundary(:,1),'LineWidth', 1, 'Color', 'g')
 end
 
-for k = 1:length(vbndries)
-    plot(vboundary(:,2), vboundary(:,1), 'w', 'LineWidth', 2, 'Color', 'r')
+for k = 1:length(vbCell)
+    vboundary=vbCell{k};
+    plot(vboundary(:,2), vboundary(:,1),'LineWidth', 1, 'Color', 'r')
 end
 
-title('Boundaries Overlayed');
+title('Dorsal(green) and Ventral(red) Boundaries Overlayed');
 end
