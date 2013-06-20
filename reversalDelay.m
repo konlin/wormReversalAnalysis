ce043fba5b78e49972231e9fae019baf97af99ab
Konlin <konlin.shen@gmail.com>
2013-06-20 17:09:10 -0400

    troubleshooting

--- reversalDelay.m
+++ reversalDelay.m
@@ -11,11 +11,14 @@ delayArray=[];
 for i=1:numFrames-1
     if(velocity(i)<0)
         time=0;
-        tempFrames=i+1;
+        tempFrames=i+1
         %look for last time DLP was on
-        while(mcdf(tempFrames).DLPisOn==0)
+        while mcdf(tempFrames).DLPisOn==0
             time=time+1;
             tempFrames=tempFrames-1;
+            if(tempFrames==0)
+                break;
+            end
         end
         delayArray=[delayArray,time];
     end
