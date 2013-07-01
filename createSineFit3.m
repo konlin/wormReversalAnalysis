fedef4dac4d5506d5497fe4a1b77c5c591288a0e
Konlin <konlin.shen@gmail.com>
2013-07-01 10:53:17 -0400

    edited sine fit for constructor

---
+++ createSineFit3.m
@@ -0,0 +1,25 @@
+function x =createSineFit3(c, options)
+%c = c(19:90);
+xdata = [1:100]';
+lb = [0; 0; 0];
+ub = [12; .18; 2*pi];
+
+x0 = [5; .097; pi];
+x = lsqcurvefit(@myfun,x0,xdata,c,lb,ub, options);
+% figure;
+% hold on;
+% plot(c, '-r');
+% plot(myfun(x,xdata), '-g'); 
+% title('Click to accept, press any key to reject');
+% hold off;
+% k=waitforbuttonpress;
+% if k==1
+%     x=[];
+% end
+% 
+% close;
+end
+
+function F = myfun(x,xdata)
+F = x(1)*sin(x(2)*xdata + x(3));
+end
